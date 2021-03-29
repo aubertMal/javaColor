209b28dbd448935effa0003357d8991cddc0f29c
aubertMal <malek.aubert@gmail.com>
2021-03-29 14:29:16 +0200

    définition des tests pour les getters/setters

--- test/aubert/color/model/ColorTest.java
+++ test/aubert/color/model/ColorTest.java
@@ -1,11 +1,18 @@
 package aubert.color.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ColorTest {
-    private Color color= new Color(153,51,102);
+    private Color color;
+
+    @Before
+    @Test
+    public void testColor(){
+        color = new Color(153,51,102);
+    }
 
     @Test
     public void testGetRed(){
@@ -20,5 +27,23 @@ public class ColorTest {
     @Test
     public void testGetBlue(){
         assertEquals(102, color.getBlue());
+    }
+
+    @Test
+    public void testSetBlue(){
+        color.setBlue(56);
+        assertEquals(56, color.getBlue());
+    }
+
+    @Test
+    public void testSetRed(){
+        color.setRed(70);
+        assertEquals(70, color.getRed());
+    }
+
+    @Test
+    public void testSetGreen(){
+        color.setGreen(70);
+        assertEquals(25, color.getGreen());
     }
 }
